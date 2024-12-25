33c36a5fa012b0507bd785f1e4f74cf06959acdf
henrrypoop <54110118+henrrypoop@users.noreply.github.com>
2024-12-25 09:19:42 +0700

    Update Matrices.py

--- Matrices.py
+++ Matrices.py
@@ -5,23 +5,23 @@ from scipy.linalg import svd
 # https://www.geeksforgeeks.org/singular-value-decomposition-svd/
 # HIGHLY UNDER DEVELOPMENT
 # a rank 2 ,2x3 matrix
-A = Matrix([[1,1,0],[0,1,1]])
-A1 = np.array([[1,1,0],[0,1,1]])
+A = Matrix([[4,1,2],[1,4,-2]])
+A1 = np.array([[4,1,2],[1,4,-2]])
 
 # a rank 1 ,2x3 matrix
-B = Matrix([[1,2,3],[2,4,6]])
-B1 = np.array([[1,2,3],[2,4,6]])
+B = Matrix([[3,3,3],[6,6,6]])
+B1 = np.array([[3,3,3],[6,6,6]])
 
 names = ["A","B"]
 # Testing
-A = Matrix([[3,2,2],[2,3,-2]])
-A1 = np.array([[3,2,2],[2,3,-2]])
+#A = Matrix([[3,2,2],[2,3,-2]])
+#A1 = np.array([[3,2,2],[2,3,-2]])
 
 def stuffs(anime,fart):
   print(f"Matrix {fart} is \n{np.array(anime)}\n")
   print(f"Reduced echelon form of matrix {fart}\n{np.array(anime)} is :\n",np.array(anime.echelon_form()),"\n")
   print(f"The rank of matrix {fart}\n{np.array(anime)} is :",anime.rank(),"\n")
-  #AxA.T Part
+  #AxA.T Pa
   print(f"{fart} x {fart}.T is:\n",np.array(anime * anime.T),"\n")
   print(f"{fart}.T x {fart} is:\n",np.array(anime.T * anime),"\n")
   print("---------------------------------------------------------------------")
@@ -34,60 +34,58 @@ def calc(Mname,matrix,name):
   A_matrix = globals()[Mname]
   print(f"matrix {Mname}\n")
   print(f"{name} is:\n",np.array(matrix))
-  print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&") 
+  print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
   #print("The det of the given matrix:",matrix.det())
   print(f"The eigenvalues and eigenvectors of {name} is:\n{matrix.eigenvects()}\n")
   #Perform diagolization
   #P are eigenvectors, D are eigenvalues
   P, D = matrix.diagonalize()
-  
+
   if name == "A*A.T" or name == "B*B.T":
     u1 = P[:,0]
     u2 = P[:,1]
     print("Let u1 be the eigenvector 1:\n",np.array(u1))
     print("Let u2 be the eigenvector 2:\n",np.array(u2),"\n")
   else:
-    v1 = P[:,0]
-    v2 = P[:,1]
-    v3 = P[:,2]
-    print("Let u1 be the eigenvector 1:\n",np.array(v1))
-    print("Let u2 be the eigenvector 2:\n",np.array(v2))
-    print("Let u3 be the eigenvector 3:\n",np.array(v3),"\n")
-
+    v1 = (P[:, 0]).T
+    v2 = (P[:, 1]).T
+    v3 = (P[:, 2]).T
+    V_1 = np.array(Matrix.vstack(v1,v2,v3))
+    a1 = V_1[0,:]
+    a2 = V_1[1,:]
+    a3 = V_1[2,:]
+    #print(a1,"\n",a2,"\n",a3,"\n")
+    #print("Testing",V_1) 
+    print("v2 is:/n",(np.dot(a2,a1)/np.dot(a1,a1))*a1)
+    print("v3 is:/n",(np.dot(a3,a2)/np.dot(a2,a2))*a2)
   #Checking for orthogonality
- 
+
   if name == "A*A.T" or name == "B*B.T":
     if u1.dot(u2) == 0:
       print("orthogonality between u1 and u2 is TRUE\n")
     else:
       print("orthogonality between u1 and u2 is FALSE\n")
-  else:
-    if v1.dot(v2) == 0 and v1.dot(v3) == 0 and v2.dot(v3) == 0:
-          print("orthogonality between u1 and u2 and u3 is TRUE\n")
-    else:
-          print("orthogonality between u1 and u2 and u3 is FALSE\n")
-
   #Normalizing the eigenvectors to form w1,|w2/ to form an orthogonal matrix that diagonalizes A.AT
   if name == "A*A.T" or name == "B*B.T":
     w1 = u1 / u1.norm()
     w2 = u2 / u2.norm()
     U_ = Matrix.hstack(w1,w2)
-    print(f"The matrix that orthogonally diagnoalizes {name} is \nU=\n",np.array(U_),"\n")
+    print(f"The matrix that orthogonally diagonalizes {name} is \nU=\n",np.array(U_),"\n")
   else: # For A.T x A or B.T x B
-    v1 = v1 / v1.norm()
-    v2 = v2 / v2.norm()
-    v3 = v3 / v3.norm()
+    v1 = v1.T / Matrix(v1).norm()
+    v2 = v2.T / Matrix(v2).norm()
+    v3 = v3.T / Matrix(v3).norm()
     V_ = Matrix.hstack(v1,v2,v3)
-    print(f"The matrix that orthogonally diagnoalizes {name} is \nV=\n",np.array(V_),"\n")
+    print(f"The matrix that orthogonally diagonalizes {name} is \nV=\n",np.array(V_),"\n")
   #Constructing Matrix U that will orthogonally diagonalizes A.AT/AT.A
   U , S, V = svd(A_matrix)
   print(f"Left singular vectors of matrix {Mname} is:\nU=\n",U,"\n")
   print(f"Singular value of matrix {Mname} is:\nS=\n",S,"\n")
   print(f"Right singular vectors of matrix {Mname} is:\nV.T=\n",V,"\n")
-  print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&") 
+  print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
 
-"""steve = A*A.T 
-alex = B*B.T 
+"""steve = A*A.T
+alex = B*B.T
 reverse_steve = A.T*A
 reverse_alex = B.T*B
 
@@ -107,4 +105,3 @@ calc("A1",A*A.T,"A*A.T")
 calc("A1",A.T*A,"A.T*A")
 calc("B1",B*B.T,"B*B.T")
 calc("B1",B.T*B,"B.T*B")
-
